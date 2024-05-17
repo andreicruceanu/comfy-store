1a9e5645231f02480f711fd8ec65e6e523c22c10
andreicruceanu <cruceanuandrei10@gmail.com>
2024-05-17 18:07:24 +0300

    Links components

--- src/App.tsx
+++ src/App.tsx
@@ -11,50 +11,67 @@ import {
   Register,
   Products,
   Orders,
+  Error,
 } from "./pages";
+import { ErrorElement } from "./components";
+
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as productsLoader } from "./pages/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
       },
       {
         path: "products",
         element: <Products />,
+        errorElement: <ErrorElement />,
+        loader: productsLoader,
       },
       {
         path: "products/:id",
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "cart",
         element: <Cart />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "about",
         element: <About />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "checkout",
         element: <Checkout />,
+        errorElement: <ErrorElement />,
       },
       {
         path: "orders",
         element: <Orders />,
+        errorElement: <ErrorElement />,
       },
     ],
   },
   {
     path: "/login",
     element: <Login />,
+    errorElement: <Error />,
   },
   {
     path: "/register",
     element: <Register />,
+    errorElement: <Error />,
   },
 ]);
 
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,4 +1,24 @@
+import { links } from "@/utils";
+import { NavLink } from "react-router-dom";
+
 function NavLinks() {
-  return <div>NavLinks</div>;
+  return (
+    <div className="hidden lg:flex justify-center items-center gap-x-4">
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            key={link.label}
+            className={({ isActive }) => {
+              return `capitalize font-normal
+               tracking-wide ${isActive ? "text-primary" : ""}`;
+            }}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }
 export default NavLinks;
