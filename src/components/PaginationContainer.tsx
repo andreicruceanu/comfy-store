02e127d936994dfecece352f8532729860014465
andreicruceanu <cruceanuandrei10@gmail.com>
2024-05-19 22:01:22 +0300

    pagination

--- src/components/PaginationContainer.tsx
+++ src/components/PaginationContainer.tsx
@@ -1,4 +1,72 @@
+import {
+  ProductsResponseWithParams,
+  constructPrevOrNextUrl,
+  constructUrl,
+} from "@/utils";
+import { useLoaderData, useLocation } from "react-router-dom";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
 function PaginationContainer() {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData() as ProductsResponseWithParams;
+  const { pageCount, page } = meta.pagination;
+
+  const { search, pathname } = useLocation();
+
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
+  if (pageCount < 2) return null;
+
+  const renderPagination = pages.map((pageNumber) => {
+    const isActive = pageNumber === page;
+    const url = constructUrl({ pageNumber, search, pathname });
+
+    return (
+      <PaginationItem key={pageNumber}>
+        <PaginationLink to={url} isActive={isActive}>
+          {pageNumber}
+        </PaginationLink>
+      </PaginationItem>
+    );
+  });
+
+  const { prevUrl, nextUrl } = constructPrevOrNextUrl({
+    currentPage: page,
+    pageCount,
+    search,
+    pathname,
+  });
+
+  return (
+    <Pagination className="mt-16">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            to={prevUrl}
+            aria-disabled={page <= 1}
+            tabIndex={page <= 1 ? -1 : undefined}
+            className={page <= 1 ? "pointer-events-none opacity-50" : undefined}
+          />
+        </PaginationItem>
+        {renderPagination}
+        <PaginationItem>
+          <PaginationNext
+            to={nextUrl}
+            aria-disabled={page >= pageCount}
+            tabIndex={page >= pageCount ? -1 : undefined}
+            className={
+              page >= pageCount ? "pointer-events-none opacity-50" : undefined
+            }
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
 }
 export default PaginationContainer;
