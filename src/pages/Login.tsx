e9189ab2ada98ce1564d860f49d1dbc5d29728f5
andreicruceanu <cruceanuandrei10@gmail.com>
2024-05-25 23:54:22 +0300

    Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,92 @@
+import { FormInput, SubmitBtn } from "@/components";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { toast } from "@/components/ui/use-toast";
+import { loginUser } from "@/features/user/userSlice";
+import { useAppDispatch } from "@/hooks";
+import { type ReduxStore } from "@/store";
+import { customFetch } from "@/utils";
+import { AxiosResponse } from "axios";
+import {
+  type ActionFunction,
+  Form,
+  Link,
+  useNavigate,
+  redirect,
+} from "react-router-dom";
+
+export const action =
+  (store: ReduxStore): ActionFunction =>
+  async ({ request }): Promise<Response | null> => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+
+    try {
+      const response: AxiosResponse = await customFetch.post(
+        "/auth/local",
+        data
+      );
+      const username = response.data.user.username;
+      const jwt = response.data.jwt;
+      store.dispatch(loginUser({ username, jwt }));
+      return redirect("/");
+    } catch (error) {
+      console.log(error);
+      toast({ description: "Login Failed" });
+      return null;
+    }
+  };
+
 function Login() {
-  return <div>Login</div>;
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async (): Promise<void> => {
+    try {
+      const response: AxiosResponse = await customFetch.post("/auth/local ", {
+        identifier: "test@test.com",
+        password: "secret",
+      });
+
+      const username = response.data.user.username;
+      const jwt = response.data.jwt;
+      dispatch(loginUser({ username, jwt }));
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast({ description: "Login Failed" });
+    }
+  };
+  return (
+    <section className="h-screen grid place-items-center">
+      <Card className="w-96 bg-muted">
+        <CardHeader>
+          <CardTitle className="text-center">Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form method="post">
+            <FormInput type="email" label="email" name="identifier" />
+            <FormInput type="password" name="password" />
+            <SubmitBtn text="Login" className="w-full mt-4" />
+            <Button
+              type="button"
+              variant="outline"
+              onClick={loginAsGuestUser}
+              className="w-full mt-4"
+            >
+              Gest user
+            </Button>
+            <p className="text-center mt-4">
+              Not a member yet?{" "}
+              <Button type="button" asChild variant="link">
+                <Link to="/register">Register</Link>
+              </Button>
+            </p>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
 export default Login;
