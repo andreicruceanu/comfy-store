7ab05c4d75be825ec022ff0ddfcc15356b3324b4
andreicruceanu <cruceanuandrei10@gmail.com>
2024-05-30 00:44:42 +0300

    checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,4 +1,36 @@
+import { CartTotals, CheckoutForm, SectionTitle } from "@/components";
+import { toast } from "@/components/ui/use-toast";
+import { useAppSelector } from "@/hooks";
+import { ReduxStore } from "@/store";
+import { LoaderFunction, redirect } from "react-router-dom";
+
+export const loader =
+  (store: ReduxStore): LoaderFunction =>
+  async (): Promise<Response | null> => {
+    const user = store.getState().userState.user;
+
+    if (!user) {
+      toast({ description: "Please login to continue" });
+      return redirect("/login");
+    }
+    return null;
+  };
+
 function Checkout() {
-  return <div>Checkout</div>;
+  const cartTotal = useAppSelector((state) => state.cartState.cartTotal);
+
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+
+  return (
+    <>
+      <SectionTitle text="Place your order" />
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 }
 export default Checkout;
